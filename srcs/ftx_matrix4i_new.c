287d8cf256ffefd991fbd2bb52a2fb90cc8dff99
jfarinha <ajoni844@gmail.com>
2020-11-01 19:21:19 +0000

    added unfinished new mat func

---
+++ srcs/ftx_matrix4i_new.c
@@ -0,0 +1,8 @@
+#include <libftx_priv.h>
+#include <stdlib.h>
+
+void	ftx_matrix4i_new(t_ftx_matrix4i *matrixtoInit)
+{
+	ft_thrower(!(*matrixtoInit = malloc(sizeof\
+	"Error could not alocate Integer 4x4 Matrix\n");
+}
