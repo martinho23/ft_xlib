fcbe0fd4eced5312ac93b794ebbb8fd0545de650
jfarinha <ajoni844@gmail.com>
2020-11-03 19:16:41 +0100

    Added Warning message for NULL matrix's in delete and print functions

--- srcs/ftx_matrix4i_delete.c
+++ srcs/ftx_matrix4i_delete.c
@@ -1,4 +1,5 @@
  #include "../includes/libftx_priv.h"
+ #include "../includes/libft.h"
  #include <stdlib.h>
 
  void   ftx_matrix4i_delete(t_ftx_matrix4i *matrixToDelete)
@@ -6,10 +7,20 @@
      int   i;
 
      i = 0;
-     while(*matrixToDelete && *matrixToDelete[i] && i < FTX_MATRIX4_LENGTH)
+     if (!(*matrixToDelete))
      {
-         free(*matrixToDelete[i]);
-         *matrixToDelete[i] = NULL;
+        ft_putstr("/!\\ WARNING: You're trying to delete a NULL t_ftx_matrix4i\n");
+        return ;
+     }
+     while(i < FTX_MATRIX4_LENGTH)
+     {
+        if (!(*matrixToDelete)[i])
+        {
+            ft_putstr("/!\\ WARNING: You're trying to delete a NULL t_ftx_matrix4i\n");
+            return ;
+        }
+         free((*matrixToDelete)[i]);
+         (*matrixToDelete)[i] = NULL;
         i++;
      }
      free(*matrixToDelete);
--- srcs/ftx_matrix4i_print.c
+++ srcs/ftx_matrix4i_print.c
@@ -1,11 +1,9 @@
 #include "../includes/libftx_priv.h"
 #include "../includes/libft.h"
 
-static  void ftx_matrix4i_print_element(int i, int j, int value, char *matrixName)
+static  void ftx_matrix4i_print_element(int i, int j, int value)
 {
-    ft_putstr("Printing matrix ");
-    ft_putstr(matrixName);
-    ft_putstr(": \nMatrix[");
+    ft_putstr("Matrix[");
     ft_putnbr(i);
     ft_putstr("][");
     ft_putnbr(j);
@@ -20,12 +18,25 @@ void    ftx_matrix4i_print(t_ftx_matrix4i matrixToPrint, char *matrixName)
     int     j;
 
     i = 0;
+    ft_putstr("Printing matrix ");
+    ft_putstr(matrixName);
+    ft_putstr(": \n");
+    if (!(matrixToPrint))
+    {
+        ft_putstr("/!\\ WARNING: You're trying to print a NULL t_ftx_matrix4i\n");
+        return ;
+    }
     while(i < FTX_MATRIX4_LENGTH)
     {
         j = 0;
-        while (matrixToPrint && matrixToPrint[i][j]  && j < FTX_MATRIX4_LENGTH)
+        while (j < FTX_MATRIX4_LENGTH)
         {
-            ftx_matrix4i_print_element(i, j, matrixToPrint[i][j], matrixName);
+            if (!(matrixToPrint[i][j]))
+            {
+                ft_putstr("/!\\ WARNING: You're trying to print a NULL t_ftx_matrix4i\n");
+                return ;
+            }
+            ftx_matrix4i_print_element(i, j, matrixToPrint[i][j]);
             j++;
         }
         i++;
