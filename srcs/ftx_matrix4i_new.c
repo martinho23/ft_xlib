55f4e9d704c652f64121709f86be7d7bca39965b
jfarinha <ajoni844@gmail.com>
2020-11-03 19:34:15 +0100

    Fixed Malloc Cast on ftx_matrix4i_new

--- srcs/ftx_matrix4i_new.c
+++ srcs/ftx_matrix4i_new.c
@@ -4,13 +4,14 @@
 
 void	ftx_matrix4i_new(t_ftx_matrix4i *matrixToInit)
 {
-	int		i;
-	ft_thrower(!(*matrixToInit = malloc(sizeof(*matrixToInit) * FTX_MATRIX4_LENGTH)),\
+	int				i;
+
+	ft_thrower(!(*matrixToInit = (int **)malloc(sizeof(int *) * FTX_MATRIX4_LENGTH)),\
 	"Error could not alocate Integer 4x4 Matrix\n");
 	i = 0;
 	while (i < FTX_MATRIX4_LENGTH)
 	{
-		ft_thrower(!(*matrixToInit[i] = malloc(sizeof(**matrixToInit) * FTX_MATRIX4_LENGTH)),\
+		ft_thrower(!((*matrixToInit)[i] = (int *)malloc(sizeof(int) * FTX_MATRIX4_LENGTH)),\
 		"Error could not alocate Integer 4x4 Matrix\n");
 		i++;
 	}
