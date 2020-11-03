89cc2ea11912b3df3b0fc53693b277f34128ab11
jfarinha <ajoni844@gmail.com>
2020-11-03 21:24:06 +0100

    fixed some prototypes

--- includes/libftx_priv.h
+++ includes/libftx_priv.h
@@ -1,6 +1,7 @@
 #ifndef LIBFTX_PRIV_H
 #define LIBFTX_PRIV_H
 
+#include <stddef.h>
 /*
 ** Matrix's DEFINES
 */
@@ -14,9 +15,9 @@
 /*MatrixN*/
 typedef int     **t_ftx_matrixni; /*Costum typedef fo a Integer Matrix*/
 
+void            ftx_matrixni_delete(t_ftx_matrixni *matrixToDelete, size_t matrixCollums); /*Delete allocated Integer NxN Matrix*/
 void            ftx_matrixni_new(t_ftx_matrixni *matrixToInit, size_t matrixCollums, size_t matrixRows); /*Allocate Integer NxN Matrix*/
-void            ftx_matrixni_print(t_ftx_matrixni *matrixToPrint, size_t matrixCollums, size_t matrixRows); /*Print a Integer NxN Matrix*/
-void            ftx_matrixni_delete(t_ftx_matrixni *matrixToDelete, size_t matrixCollums, size_t matrixRows); /*Delete allocated Integer NxN Matrix*/
+void            ftx_matrixni_print(t_ftx_matrixni matrixToPrint, char *matricName, size_t matrixCollums, size_t matrixRows); /*Print a Integer NxN Matrix*/
 
 t_ftx_matrixni  ftx_matrixni_new_by_return(size_t matrixCollums, size_t matrixRows); /*Allocate Integer NxN Matrix by reference*/
 
@@ -24,8 +25,8 @@ t_ftx_matrixni  ftx_matrixni_new_by_return(size_t matrixCollums, size_t matrixRo
 typedef int     **t_ftx_matrix4i; /*Costum typedef for a Integers 4x4 Matrix*/
 
 void			ftx_matrix4i_new(t_ftx_matrix4i *matrixToInit); /*Allocate Integer 4x4 Matrix*/
-void			ftx_matrix4i_print(t_ftx_matrix4i matrixToPrint, char *matrixName); /*Print a Integer 4x4 Matrix*/
 void			ftx_matrix4i_delete(t_ftx_matrix4i *matrixToDelete); /*Delete allocated Integer 4x4 Matrix*/
+void			ftx_matrix4i_print(t_ftx_matrix4i matrixToPrint, char *matrixName); /*Print a Integer 4x4 Matrix*/
 
 t_ftx_matrix4i	ftx_matrix4i_new_by_return(void); /*Allocate Integer 4x4 Matrix by reference*/
 
