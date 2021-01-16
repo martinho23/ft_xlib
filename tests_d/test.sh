3084eafd563e40fa229f589da7f966aa0ca20843
jfarinha <ajoni844@gmail.com>
2021-01-16 17:56:36 +0000

    Fixed some bugs on the test unit compilation

--- tests_d/test.sh
+++ tests_d/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 MAKE_PWD=$(find $(pwd) -name "tests_d")
-EXECS=$(find $MAKE_PWD -name "*.exe")
 make -C $MAKE_PWD
+EXECS=$(find $MAKE_PWD -name "*.exe")
 $EXECS > output.txt; if [ $(cat output.txt | grep "###" | wc -l) = $(expr $(echo -n $(cat output.txt | grep "Tests run:" | cut -d " " -f 3) | sed -e s/' '/' + '/g)) ]
 then
 	echo ok
